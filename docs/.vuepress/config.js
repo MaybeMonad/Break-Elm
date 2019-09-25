177d5fb0f893f28e8c5fdbf82bd723ef18d71093
Leo <dezineleo@gmail.com>
2019-09-25 15:17:43 +0800

    init commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: 'Hello Elm',
+  description: 'Just playing around',
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '文档', link: '/guide/' },
+      { text: '官方', link: 'https://elm-lang.org' },
+    ],
+  },
+  displayAllHeaders: true // 默认值：false
+}
